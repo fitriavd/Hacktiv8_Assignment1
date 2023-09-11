d1c08a4e4eadf199d3fd9bc3c365802ffa5fd9d6
fitriavd <fitriavuspita@gmail.com>
2023-09-11 08:45:57 +0700

    Assigment1

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function validateForm() {
+    var nama = document.getElementById("nama").ariaValueMax;
+    var role = document.getElementById("role").ariaValueMax;
+    var availability = document.getElementById("availability").ariaValueMax;
+    var name = document.getElementById("nama").ariaValueMax;
+    var name = document.getElementById("nama").ariaValueMax;
+    var name = document.getElementById("nama").ariaValueMax;
+    var name = document.getElementById("nama").ariaValueMax;
+    var name = document.getElementById("nama").ariaValueMax;
+}
